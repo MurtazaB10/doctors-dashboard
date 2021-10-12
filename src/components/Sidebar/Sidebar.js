91d104ff696d29984722146328d9ce3a41f0c875
AMDesigners <amdesigners52@gmail.com>
2021-10-12 16:35:12 +0530

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -39,13 +39,9 @@ const Sidebar = () => {
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Expense"
-            >
-              <i className="mdi mdi-cash-multiple menu-icon" />
-              <span className="menu-title">Expenses</span>
+            <NavLink activeClassName="active" className="nav-link" to="/PIQ">
+              <i className="mdi mdi-file-check menu-icon" />
+              <span className="menu-title">PIQ</span>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -56,66 +52,6 @@ const Sidebar = () => {
             >
               <i className="mdi mdi-cash menu-icon" />
               <span className="menu-title">Commission</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Account"
-            >
-              <i class="far fa-user"></i>
-              <span className="menu-title">Account</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Productlist"
-            >
-              <i className="mdi mdi-package menu-icon" />
-              <span className="menu-title">Product</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Services"
-            >
-              <i className="mdi mdi-settings-box menu-icon" />
-              <span className="menu-title">Services</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Prescription"
-            >
-              <i className="mdi mdi-file-document-box menu-icon" />
-              <span className="menu-title">Prescription</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Laboratory"
-            >
-              <i className="mdi mdi-hospital-building menu-icon" />
-              <span className="menu-title">Laboratory</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeClassName="active"
-              className="nav-link"
-              to="/Designation"
-            >
-              <i class="far fa-address-card"></i>
-              <span className="menu-title">Designation</span>
             </NavLink>
           </li>
           <li className="nav-item">
--- src/routes/BasicRoutes.js
+++ src/routes/BasicRoutes.js
@@ -3,18 +3,13 @@ import { Switch } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Appointment from "../pages/Appointment/Appointment";
 import Patient from "../pages/Patient/Patient";
-import Expense from "../pages/Expense/Expense";
+import PIQ from "../pages/PIQ/PIQ";
 import Commission from "../pages/Commision/Commission";
-import Services from "../pages/Services/Services";
-import Prescription from "../pages/Prescription/Prescription";
-import Laboratory from "../pages/Laboratory/Laboratory";
-import ProductList from "../pages/Product/ProductList";
 import Login from "../components/Auth/Login";
 import LoginLayoutRoute from "./LoginLayoutRoutes";
 import BasicLayout from "../layout/BasicLayout";
 import LoginLayout from "../layout/LoginLayout";
 import LoginRoutes from "./LoginRoutes";
-import Account from "../pages/Account/Account";
 
 const BasicRoutes = () => {
   return (
@@ -26,6 +21,7 @@ const BasicRoutes = () => {
           return user ? <Redirect to="/Dashboard" /> : <Redirect to="/Login" />;
         }}
       /> */}
+      <LoginLayoutRoute path="/PIQ" component={PIQ} layout={BasicLayout} />
       <LoginLayoutRoute
         exact
         path="/"
@@ -44,43 +40,8 @@ const BasicRoutes = () => {
         layout={BasicLayout}
       />
       <LoginLayoutRoute
-        path="/Expense"
-        component={Expense}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
         path="/Commission"
         component={Commission}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Account"
-        component={Account}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Productlist"
-        component={ProductList}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Services"
-        component={Services}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Prescription"
-        component={Prescription}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Laboratory"
-        component={Laboratory}
-        layout={BasicLayout}
-      />
-      <LoginLayoutRoute
-        path="/Designation"
-        component={Appointment}
         layout={BasicLayout}
       />
       <LoginRoutes path="/Login" component={Login} layout={LoginLayout} />
